80a9b1fc554d56f666d66a92b0e9e9fedb501752
Balakumar Balasundaram <balakumar.b05@gmail.com>
2022-03-30 17:16:00 +0200

    added logger file

--- src/main/java/org/bk/javalearnapi/datetime/UtilTimeOperation.java
+++ src/main/java/org/bk/javalearnapi/datetime/UtilTimeOperation.java
@@ -1,4 +1,17 @@
 package org.bk.javalearnapi.datetime;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
 public class UtilTimeOperation {
+    public static final Logger logger = LogManager.getLogger(UtilTimeOperation.class);
+
+    public static void main(String[] args) {
+        Timestamp instant = Timestamp.from(Instant.now());
+        logger.info("current time is {}", instant);
+    }
+
 }
