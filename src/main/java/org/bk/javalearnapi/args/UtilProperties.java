0e886bfb87619dff62318043acc6d8842f7fc4e6
Balakumar Balasundaram <balakumar.b05@gmail.com>
2022-03-30 17:26:07 +0200

    added logger file

--- src/main/java/org/bk/javalearnapi/args/UtilProperties.java
+++ src/main/java/org/bk/javalearnapi/args/UtilProperties.java
@@ -1,4 +1,23 @@
 package org.bk.javalearnapi.args;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class UtilProperties {
+
+    public static final Logger logger = LogManager.getLogger(UtilProperties.class);
+
+    public static void main(String[] args) throws IOException {
+
+        Properties props = new Properties();
+        InputStream input = UtilProperties.class.getClassLoader().getResourceAsStream("config.properties");
+        props.load(input);
+
+        logger.info("topic name : {} ", props.getProperty("topicName"));
+
+    }
 }
